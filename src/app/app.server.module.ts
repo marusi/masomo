a13b59c613ab04439222bd15f3bd5b173f3af324
marusi <edgargamarusi@live.com>
2021-02-25 05:17:21 +0300

    omissions
    omission

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -1,12 +1,8 @@
-import 'zone.js/dist/zone-node';
+
 
 import { enableProdMode } from '@angular/core'
 
 
 enableProdMode();
-const args = process.argv.slice(2);
-if (args.length != 1) {
-    process.stdout.write("Usage: node dist/main.js <url>");
-    process.exit();
-}
+
 
