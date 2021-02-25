2d3c870a3286fc7418e316656f036f7f5725eb17
marusi <edgargamarusi@live.com>
2021-02-25 05:11:54 +0300

    updated app.server
    do it

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -1,7 +1,7 @@
 import 'zone.js/dist/zone-node';
-import { renderModuleFactory } from '@angular/platform-server'
+
 import { enableProdMode } from '@angular/core'
-import { AppServerModuleNgFactory } from './src/app.server.module.ngfactory'
+
 
 enableProdMode();
 const args = process.argv.slice(2);
@@ -9,19 +9,4 @@ if (args.length != 1) {
     process.stdout.write("Usage: node dist/main.js <url>");
     process.exit();
 }
-renderModuleFactory(AppServerModuleNgFactory, {
-    document: `<!doctype html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <title>NgUniversalDemystified</title>
-  <base href="/">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon" type="image/x-icon" href="favicon.ico">
-</head>
-<body>
-  <app-root>Loading...</app-root>
-</body>
-</html>`,
-    url: args[0]
-}).then(string => process.stdout.write(string));
+
